110300cbcfac64f9046e257c8773e4e491b3c516
LUFFPUFF <beatsluffi@gmail.com>
2024-11-13 19:07:25 +0300

    fix bag config path

--- src/main/java/logic/config/filepath/FilePath.java
+++ src/main/java/logic/config/filepath/FilePath.java
@@ -1,7 +1,23 @@
 package logic.config.filepath;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FilePath {
 
-    public static final String FILE_PATH_MARKOV_CHAIN = "src/main/java/output/markov_chain.png";
-    public static final String FILE_PATH_STRING_CONTENT = "src/main/java/output/example.txt";
+    public static final String FILE_PATH_MARKOV_CHAIN = getOutputPath("markov_chain.png");
+    public static final String FILE_PATH_STRING_CONTENT = getOutputPath("example.txt");
+
+    private static String getOutputPath(String filename) {
+        Path outputPath = Paths.get("src", "main", "java", "output");
+        try {
+            Files.createDirectories(outputPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create output directory", e);
+        }
+
+        return outputPath.resolve(filename).toString();
+    }
 }
